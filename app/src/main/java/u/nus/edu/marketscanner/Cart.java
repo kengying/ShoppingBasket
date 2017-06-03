9bf82172516203203a915fc97cff11fb2d6e6f9e
srishag <srishti.23997@gmail.com>
2017-06-04 01:23:11 +0800

    trying again

--- app/src/main/java/u/nus/edu/marketscanner/Cart.java
+++ app/src/main/java/u/nus/edu/marketscanner/Cart.java
@@ -13,7 +13,7 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 /**
- * Created by PANDA (and edited by srishti) on 31/5/2017.
+ * Created by PANDA (and srish) on 31/5/2017.
  */
 
 public class Cart extends AppCompatActivity {
@@ -97,6 +97,8 @@ public class Cart extends AppCompatActivity {
         }
 
     }
+
+    /* trying to edit */
 
     @Override
     public void onBackPressed()
