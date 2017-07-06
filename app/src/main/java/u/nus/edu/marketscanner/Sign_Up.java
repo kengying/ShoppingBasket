9be47c7adc5788f1f71287281c2004a798feb21b
srishag <srishti.23997@gmail.com>
2017-07-06 12:37:12 +0800

    confirmed layout of sign up page

--- app/src/main/java/u/nus/edu/marketscanner/Sign_Up.java
+++ app/src/main/java/u/nus/edu/marketscanner/Sign_Up.java
@@ -1,26 +1,11 @@
 package u.nus.edu.marketscanner;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 /**
- * Created by PANDA on 14/6/2017.
+ * Created by ithsirslawragga on 6/7/17.
  */
 
 public class Sign_Up extends AppCompatActivity {
 
-    private Button signUpBtn = null;
-    protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.signup);
-        super.onCreate(savedInstanceState);
-    }
 }
