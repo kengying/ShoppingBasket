c0a6429ee592057a66c901c1a1c8c5979ea36d47
srishag <srishti.23997@gmail.com>
2017-07-09 18:43:47 +0800

    adds user to database then returns to login page

--- app/src/main/java/u/nus/edu/marketscanner/Sign_Up.java
+++ app/src/main/java/u/nus/edu/marketscanner/Sign_Up.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by ithsirslawragga on 6/7/17.
@@ -23,10 +27,14 @@ public class Sign_Up extends AppCompatActivity {
     boolean valid = true;
     private User user;
 
+    DatabaseReference databaseReference;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setCancelable(false);
@@ -46,7 +54,14 @@ public class Sign_Up extends AppCompatActivity {
                 valid = validate(userName.getText().toString(), firstName.getText().toString(), lastName.getText().toString(),
                         password.getText().toString(), cfmPassword.getText().toString());
 
-                //TODO: add ability to add user to Firebase
+                if(valid) {
+                    Log.d("adding ", "pending");
+                    addUser();
+
+                    Intent i = new Intent(Sign_Up.this, Login.class);
+                    startActivity(i);
+                    finish();
+                }
             }
         });
 
@@ -60,31 +75,45 @@ public class Sign_Up extends AppCompatActivity {
         });
     }
 
+    private void addUser() {
+        String SuserName = userName.getText().toString();
+        String SfirstName = firstName.getText().toString();
+        String SlastName = lastName.getText().toString();
+        String Spassword = password.getText().toString();
+
+        String id = databaseReference.push().getKey();
+        user = new User(SuserName, Spassword, SlastName, SfirstName, (long) 0);
+
+        databaseReference.child(id).setValue(user);
+        Toast.makeText(getApplicationContext(), "Sign up successful", Toast.LENGTH_SHORT).show();
+    }
+
     public boolean validate(final String SuserName, final String SfirstName, final String SlastName, final String Spassword,
                          final String ScfmPassword) {
 
         if (SuserName.isEmpty() || (SuserName.length() < 3)) {
             Toast.makeText(getApplicationContext(), "Username must be at least 3 characters", Toast.LENGTH_SHORT).show();
             valid = false;
+            userName.setError(null);
         } else if (SfirstName.isEmpty() || SlastName.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Fill in all fields", Toast.LENGTH_SHORT).show();
             valid = false;
+            firstName.setError(null);
+            lastName.setError(null);
         } else if (Spassword.isEmpty() || (Spassword.length() < 7)) {
             //ERROR: 7 characters is not allowed, only 8 or more is allowed
             Toast.makeText(getApplicationContext(), "Password must be at least 8 characters", Toast.LENGTH_SHORT).show();
             valid = false;
+            password.setError(null);
+            Log.d("password ", "pending");
         } else if (ScfmPassword.isEmpty() || (!Spassword.matches(ScfmPassword))) {
             Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
             valid = false;
+            cfmPassword.setError(null);
+            Log.d("cfm password ", "pending");
         }
 
         //TODO: add if username is already being used - set valid to false
-
-            userName.setError(null);
-            firstName.setError(null);
-            lastName.setError(null);
-            password.setError(null);
-            cfmPassword.setError(null);
 
         return valid;
     }
